9ed8a8065f3cd49e5e922d6551773b2ae1b33088
mohsenrh214 <mohsen.rh0214@gmail.com>
2022-03-15 12:44:19 +0330

    added pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserResults from '../component/users/UserResults';
+
+function Home() {
+	return (
+		<React.Fragment>
+			<UserResults />
+		</React.Fragment>
+	);
+}
+
+export default Home;
